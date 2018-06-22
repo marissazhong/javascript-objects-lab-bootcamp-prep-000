47abddbe3295a8fa720977d4f577372fc0786933
Marissa Zhong <mwz2101@columbia.edu>
2018-06-22 15:21:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {
+  
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
